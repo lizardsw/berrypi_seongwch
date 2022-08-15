b8817fcb93ccf40384b98b563c0f0d6bc39da776
성욱최 <lizardsw@seong-ugchoeui-MacBookAir.local>
2022-08-15 18:14:34 +0900

    socket

--- process/servo.py
+++ process/servo.py
@@ -10,10 +10,19 @@ def init_socket(s):
     end = s.recv(1024).decode('utf-8')
     print(end)
 
+def dict_to_str(my_dict):
+	dict_str = ""
+	for x, y in my_dict.items() :
+		dict_str += str(x)
+		dict_str += "="
+		dict_str += str(y)
+		dict_str += ";"
+	return (dict_str)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	init_socket(s)
 	while True :
 		data = s.recv(1024).decode('utf-8')
 		print(data)
+
 
