5242214237064e5f13d84dc24bcc65cd2ef81d1b
pi-seongwch <lizardsw@naver.com>
2022-08-30 17:39:33 +0900

    time_for

--- process/time_process.py
+++ process/time_process.py
@@ -20,30 +20,28 @@ def check_time(time_str) :
 	dateformat = "%Y-%m-%d %H:%M:%S"
 	time1 = datetime.datetime.now()
 	now_time = time1.strftime(dateformat)
+	now_time = datetime.datetime.strptime(now_time, dateformat)
 	time2 = datetime.datetime.strptime(time_str, dateformat)
 	time_diff = (now_time - time2).seconds / 60
+	print()
 	return (time_diff)
 
-def check_input_data():
+def check_input_data(s):
 	fd_read = open("./input_data.csv") # data_input fd 값 open read
 	data = pd.read_csv(fd_read, index_col = 'time')
 	time_str = data.index[-1]
 	fd_read.close
-	print(time_str)
-	time_diff = str(check_time(time_str))
-	# s.sendall(time_diff.encode('utf-8'))
+	time_diff = check_time(time_str)
+	print(time_diff)
+	s.sendall(time_diff.encode('utf-8'))
 
-schedule.every(4).seconds.do(check_input_data)
 
-while True:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s :
+	init_socket(s)
+	schedule.every(4).seconds.do(check_input_data, s)
+	#schedule.every(1).minutes.do(check_input_data, s)
+	while True:
 		schedule.run_pending()
 		time.sleep(1)
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s :
-# 	init_socket(s)
-# 	schedule.every(4).seconds.do(check_input_data, s)
-# 	#schedule.every(1).minutes.do(check_input_data, s)
-# 	while True:
-# 		schedule.run_pending()
-# 		time.sleep(1)
 
 
